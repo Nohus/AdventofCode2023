882d1f241f81df39d4a49b5a2ff5e69f173c91c1
Nohus <git@nohus.eu>
2023-12-01 06:33:21 +0000

    Day 1

--- src/main/kotlin/day01/Day1_1.kt
+++ src/main/kotlin/day01/Day1_1.kt
@@ -3,5 +3,7 @@ package day01
 import solve
 
 fun main() = solve { lines ->
-
+    lines.sumOf { line ->
+        line.filter { it.isDigit() }.let { "${it.first()}${it.last()}" }.toInt()
+    }
 }
--- src/main/kotlin/day01/Day1_2.kt
+++ src/main/kotlin/day01/Day1_2.kt
@@ -3,5 +3,10 @@ package day01
 import solve
 
 fun main() = solve { lines ->
-
+    val digits = listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    lines.sumOf { line ->
+        line.indices.mapNotNull { i ->
+            digits.indexOfFirst { line.drop(i).startsWith(it) }.takeIf { it > -1 } ?: line[i].digitToIntOrNull()
+        }.let { it.first() * 10 + it.last() }
+    }
 }
